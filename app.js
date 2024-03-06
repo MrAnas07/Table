d4768d83e58bcca919f2dba81f1afb3f40034468
MrAnas07 <anusamir45@gamil.com>
2024-03-06 18:46:18 +0500

    User Input Table

---
+++ app.js
@@ -0,0 +1,50 @@
+// var num = prompt("Enter Table ");
+// var num2 = prompt("Enter A Table Number ");
+
+// for (var i = 1; i <= num2; i++) {
+//     document.write(num + "x" + i + "=" + num*i + "<br>")
+// }
+
+
+
+
+
+
+var num = prompt("Enter Table");
+var num2 = prompt("Enter A Table Number");
+
+
+for (var i = 1; i <= num2; i++) {
+    document.write(num + "x" + i + "=" + num * i + "<br>")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
